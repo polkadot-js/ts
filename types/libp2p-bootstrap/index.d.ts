9b75a2c084a6aa15013c897e5f2df0c5faa3172d
Jaco Greeff <jacogr@gmail.com>
2018-08-23 14:35:17 +0200

    libp2p-railing -> libp2p-bootstrap

--- types/libp2p-bootstrap/index.d.ts
+++ types/libp2p-bootstrap/index.d.ts
@@ -1,12 +1,12 @@
 // Type definitions for libp2p-railing 0.9.1
-// Project: https://github.com/libp2p/js-libp2p-railing
+// Project: https://github.com/libp2p/js-libp2p-bootstrap
 // Definitions by: Jaco Greeff <https://github.com/jacogr>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
 /// <reference types="multiaddr"/>
 /// <reference types="peer-info"/>
 
-declare namespace LibP2pRailing {
+declare namespace LibP2pBootstrap {
   type Options = {
     list: Array<string | Multiaddr.Multiaddr>,
     interval?: number
@@ -16,11 +16,11 @@ declare namespace LibP2pRailing {
 }
 
 declare class LibP2pRailing {
-  constructor (options: LibP2pRailing.Options);
+  constructor (options: LibP2pBootstrap.Options);
 
-  on (event: LibP2pRailing.Events, cb: (peerInfo: PeerInfo) => any): void;
+  on (event: LibP2pBootstrap.Events, cb: (peerInfo: PeerInfo) => any): void;
 }
 
-declare module 'libp2p-railing' {
-export default LibP2pRailing;
+declare module 'libp2p-bootstrap' {
+export default LibP2pBootstrap;
 }
