750b59a59a537723f6ec58e39964a81d559dd809
Jaco Greeff <jacogr@gmail.com>
2018-09-02 09:42:27 +0200

    Add noqsl-leveldb

---
+++ types/nosql-leveldb/index.d.ts
@@ -0,0 +1,38 @@
+// Type definitions for nosql-leveldb 3.0.1
+// Project: https://github.com/snowyu/node-nosql-leveldb
+// Definitions by: Jaco Greeff <https://github.com/jacogr>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+/// <reference types="node"/>
+
+declare module 'nosql-leveldb' {
+    type LevelDbOptions$Open = {
+        createIfMissing?: boolean,
+        errorIfExists?: boolean,
+        compression?: boolean,
+        cacheSize?: boolean
+    };
+
+    type LevelDbOptions$Get ={
+        asBuffer?: boolean,
+        fillCache?: boolean
+    };
+
+    type LevelDbOptions$Put ={
+        sync?: boolean
+    };
+
+    interface LevelDb {
+        openSync (options?: LevelDbOptions$Open): void;
+        closeSync (): void;
+
+        delSync (key: Buffer): void;
+        getSync (key: Buffer, options?: LevelDbOptions$Get): Buffer;
+        putSync (key: Buffer, value: Buffer, options?: LevelDbOptions$Put): void;
+    }
+
+
+    const leveldb: (location: string) => LevelDb;
+
+    export default leveldb;
+}
