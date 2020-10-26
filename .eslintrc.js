bb651a2dee462da08e9667437a360c7020221d30
Jaco Greeff <jacogr@gmail.com>
2020-10-26 10:52:42 +0100

    Bump deps, cleanup (#108)

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,14 @@ const base = require('@polkadot/dev/config/eslint');
 
 module.exports = {
   ...base,
+  ignorePatterns: [
+    '.github/**',
+    '.vscode/**',
+    '.yarn/**',
+    '**/build/*',
+    '**/coverage/*',
+    '**/node_modules/*'
+  ],
   parserOptions: {
     ...base.parserOptions,
     project: [
