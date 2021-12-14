134e23786702ac89eb25d666de7a71e633ccd613
Jaco <jacogr@gmail.com>
2021-12-14 07:41:16 +0200

    Adjust

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,7 @@ const base = require('@polkadot/dev/config/eslint.cjs');
 module.exports = {
   ...base,
   ignorePatterns: [
-    '.github/**',
-    '.vscode/**',
-    '.yarn/**',
-    '**/build/*',
-    '**/coverage/*',
-    '**/node_modules/*'
+    ...base.ignorePatterns
   ],
   parserOptions: {
     ...base.parserOptions,
